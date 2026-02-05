0252d4dad41940f82295ac0f50ec00fa49d1d078
huevangxp <huevang933@gmail.com>
2026-02-05 15:52:35 +0700

    commit Thu 02/05/2026 15:52:35.14

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import ElectionMap from "@/components/ElectionMap";
 import { electionData, ProvinceElectionData } from "@/lib/election-data";
